91d39129c3f3edd0444ee6cfe4d84f1412f6f796
Trineathran <32535611+Trineathran@users.noreply.github.com>
2018-02-25 20:13:30 +0000

    initial Commit

---
+++ index.js
@@ -0,0 +1,34 @@
+{
+  "languageModel": {
+    "invocationName": "SivapithanChants",
+    "intents": [
+      {
+        "name": "AMAZON.CancelIntent",
+        "slots": [],
+        "samples": []
+      },
+      {
+        "name": "AMAZON.HelpIntent",
+        "slots": [],
+        "samples": []
+      },
+      {
+        "name": "AMAZON.StopIntent",
+        "slots": [],
+        "samples": []
+      },
+      {
+      "name": "ChantingIntent",
+      "slots": [            
+        {
+          "name": "Chantings",
+          "type": "LIST_OF_CHANTS"
+        }
+      ],
+	"samples":[]
+    }	
+}
+      
+   
+      
+}
